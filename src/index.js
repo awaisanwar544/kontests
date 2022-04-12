c890fdfe2e4f102a62078c68d879fc4543635aaf
awaisanwar544 <awaisanwar544@gmail.com>
2022-04-13 00:52:41 +0500

    update index with ReactDom

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
 import store from './redux/configureStore';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
