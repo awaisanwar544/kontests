6369258cb603b56886464e2652df2997c585be6d
awaisanwar544 <awaisanwar544@gmail.com>
2022-04-13 01:07:37 +0500

    update react version

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
 import store from './redux/configureStore';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root'),
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
 );
